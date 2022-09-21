49c1164fa9f892e9149f2df0d95e81e893b0a98e
ChairC <974833488@qq.com>
2022-09-21 11:32:23 +0800

    Fix: Fix the problem that the remote server can't find the module.

--- tools/__init__.py
+++ tools/__init__.py
@@ -5,6 +5,10 @@
     @Author : chairc
     @Site   : https://github.com/chairc
 """
+# 防止上远程服务器后找不到路径
+import os
+import sys
+sys.path.append(os.path.dirname(sys.path[0]))
 # from .annotations import *
 # from .demo import *
 # from .eval import *
